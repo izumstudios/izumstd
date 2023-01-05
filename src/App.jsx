923df8437f7be52122eaf5ba56a9d49f7c9c5bc4
Zheka <Zh3kaSel@yandex.ru>
2023-01-05 20:05:01 +0300

    fix offers

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import Offer3 from './components/Screens/Offer3'
 import Offer4 from './components/Screens/Offer4'
 import Offer5 from './components/Screens/Offer5'
 import Offer6 from './components/Screens/Offer6'
+import Offer7 from './components/Screens/Offer7'
+import Offer8 from './components/Screens/Offer8'
+import Offer9 from './components/Screens/Offer9'
+import Offer10 from './components/Screens/Offer10'
 
 function App() {
 	return (
@@ -18,9 +22,10 @@ function App() {
 				<Route id='4' path='/004' element={<Offer4 />} />
 				<Route id='5' path='/005' element={<Offer5 />} />
 				<Route id='6' path='/006' element={<Offer6 />} />
-				<Route id='7' path='/007' element={<Offer6 />} />
-				<Route id='8' path='/008' element={<Offer6 />} />
-				<Route id='9' path='/009' element={<Offer6 />} />
+				<Route id='7' path='/007' element={<Offer7 />} />
+				<Route id='8' path='/008' element={<Offer8 />} />
+				<Route id='9' path='/009' element={<Offer9 />} />
+				<Route id='9' path='/010' element={<Offer10 />} />
 			</Routes>
 		</Router>
 	)
